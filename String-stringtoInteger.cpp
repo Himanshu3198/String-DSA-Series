4c653ceaf3c95840a9266b60c3fcbf06f2714664
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-02-13 22:14:27 +0530

    Add files via upload

---
+++ String-stringtoInteger.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string str="42";
+    int num=stoi(str);
+
+    if(num>40){
+        cout<<"yes converted \n";
+    }
+    else{
+        cout<<"not converted\n";
+    }
+    cout<<num;
+}
