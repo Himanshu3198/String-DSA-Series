cdeeeb0315be8827d24b1a9fbc4d604da7de1f3c
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-06-22 23:46:48 +0530

    Create String-Basic-calculator-ii.cpp

---
+++ String-Basic-calculator-ii.cpp
@@ -0,0 +1,71 @@
+
+// 227. Basic Calculator II
+
+class Solution {
+public:
+    int getPriority(char &c) {
+        if(c == '+' || c == '-')
+            return 0;
+        else
+            return 1;
+    }
+    
+    int perform_op(char &c, int &a, int &b) {
+        if(c == '+')
+            return a + b;
+        else if(c == '-')
+            return a - b;
+        else if(c == '*')
+            return a * b;
+        else
+            return a / b;
+    }
+    
+    int calculate(string s) {
+        stack<int> nums;
+        stack<char> ops;
+        
+        for(int i = 0; i < s.size(); ) {
+            // skip spaces
+            while(i < s.size() && s[i] == ' ') 
+                ++i;
+            
+            // a digit found, traverse till the end of potential end of number
+            if(i < s.size() && isdigit(s[i])) {
+                int number = 0;
+                while(i < s.size() && isdigit(s[i])) {
+                    number = number * 10 + (s[i] - '0');
+                    ++i;
+                }
+                nums.emplace(number);
+            }
+            // if the current char is an operator
+            else if(i < s.size() ) {
+                // A lower priority op can't be put
+                // before finishing a higher priority op
+                // S: *, + ====> * should be performed first
+                // S: +, * ====> Allowed
+                while(!ops.empty() && getPriority(s[i]) <= getPriority(ops.top())) {
+                    // perform the operation and put the sub expression result
+                    int second = nums.top(); nums.pop();
+                    int first = nums.top(); nums.pop();
+                    nums.emplace(perform_op(ops.top(), first, second));
+                    ops.pop();
+                }
+                ops.emplace(s[i]);
+                ++i;
+            }
+        }
+        
+        // evaluate the expression with the remaining operators
+        while(!ops.empty()) {
+            // perform the operation and put the sub expression result
+            int second = nums.top(); nums.pop();
+            int first = nums.top(); nums.pop();
+            nums.emplace(perform_op(ops.top(), first, second));
+            ops.pop();
+        }
+        
+        return nums.top();
+    }
+};
