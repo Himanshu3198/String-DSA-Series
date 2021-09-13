9b914ac8b7706ae558acb1745671c1916d863098
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-09-13 12:58:59 +0530

    Add files via upload

---
+++ CF-Globla-round-16-b.cpp
@@ -0,0 +1,80 @@
+
+//  Codeforces Global Round 16 B. MIN-MEX Cut
+#include <bits/stdc++.h>
+#define ll long long int
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, n) for (int i = (n); i >= 0; i--)
+#define fst first
+#define snd second
+#define E "\n"
+#define pb push_backCF
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007LL
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define setbits(x) __builtin_popcount(x)
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+using namespace std;
+
+int main()
+{
+    fastio();
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+
+   
+
+       string s;
+       cin>>s;
+       int n=s.length();
+       int one=0,zero=0,cnt=0;
+
+
+        fr(i,n){
+
+            if(s[i]=='1') one++;
+            if(s[i]=='0') zero++;
+
+            if(s[i]=='0'){
+                    cnt++;
+                while(i<n and s[i]=='0'){
+                    i++;
+                }
+            }
+        }
+
+
+        if(one==n){
+            puts("0");
+        }
+        else if(zero==n){
+            puts("1");
+        }
+        else if(cnt>=2){
+            puts("2");
+        }
+        else{
+            puts("1");
+        }
+
+
+
+    }
+
+    return 0;
+     
+     
+  }
+  
+   
+
+
+   
