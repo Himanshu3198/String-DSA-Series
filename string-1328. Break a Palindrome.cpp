d500ff970a1cb78be085c500ed0b9d374b870228
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-10-10 23:48:54 +0530

    Update string-1328. Break a Palindrome.cpp

--- string-1328. Break a Palindrome.cpp
+++ string-1328. Break a Palindrome.cpp
@@ -1,38 +1,20 @@
 class Solution {
-public:
-    string breakPalindrome(string p) {
+    public String breakPalindrome(String pal) {
+     
         
-        int n=p.length();
-        if(n<2){
+        char s[]=pal.toCharArray();
+        int n=s.length;
+        if(n==1) return "";
+        boolean flag=true;
+        for(int i=0;i<n;i++){
             
-            return "";
-        }
-        
-         
-      
-      
-        
-         bool op=false;
-        for(int i=0;i<n/2;i++){
-            
-                  if(p[i]!='a'){
-                
-                p[i]='a';
-                op=true;
-                
+            if(s[i]!='a' && i!=n/2){
+                s[i]='a';
+                flag=false;
                 break;
             }
         }
-        
-        if(!op){
-            
-            p[n-1]='b';
-            
-        }
-        
-        return p;
-        
-        
-          
+        if(flag) s[n-1]='b';
+        return String.valueOf(s);
     }
-};
+}
